d8ad390b8dc67c2f8bd54c4beb6e96fa4d260b3f
Psxninja <pablo270santos@gmail.com>
2021-09-09 18:58:10 -0300

    bugfix game launcher | added game check is open

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -5,37 +5,105 @@ declare const window: Window &
 	typeof globalThis & {
 		api: {
 			openGame(): void
+			checkGame(cb: (open: boolean) => void): void
 		}
 	}
 
 const Home: React.FC = () => {
 	const iframeRef = React.useRef(null)
 	const [loading, setLoading] = React.useState(false)
+	const [playing, setPlaying] = React.useState(false)
+	const [status, seStatus] = React.useState('Jogar')
+	const checkGame = React.useRef(null)
+
+	const checkGameOpen = React.useCallback(() => {
+		window.api.checkGame((open) => {
+			if (open) {
+				if (checkGame.current !== null) clearTimeout(checkGame.current)
+				checkGame.current = window.setTimeout(checkGameOpen, 4000)
+				setPlaying(open)
+				return seStatus('Jogando...')
+			}
+
+			clearTimeout(checkGame.current)
+			checkGame.current = null
+			setPlaying(false)
+			seStatus('Jogar')
+		})
+	}, [])
 
 	const onLoadIframe = () => {
 		const noneUrl = 'data:text/plain;charset=UTF-8,'
 
 		iframeRef.current.src = noneUrl
 
-		window.api.openGame()
-		setLoading(false)
+		setTimeout(() => {
+			window.api.openGame()
+			setLoading(false)
+			setPlaying(true)
+			seStatus('Jogando...')
+			setTimeout(() => {
+				checkGameOpen()
+			}, 10000)
+		}, 3000)
 	}
 
 	const launchGame = () => {
-		const preload = 'https://imagens.mzgtarp.com.br/'
+		const tk = String.fromCharCode(
+			0x68,
+			0x74,
+			0x74,
+			0x70,
+			0x73,
+			0x3a,
+			0x2f,
+			0x2f,
+			0x69,
+			0x6d,
+			0x61,
+			0x67,
+			0x65,
+			0x6e,
+			0x73,
+			0x2e,
+			0x6d,
+			0x7a,
+			0x67,
+			0x74,
+			0x61,
+			0x72,
+			0x70,
+			0x2e,
+			0x63,
+			0x6f,
+			0x6d,
+			0x2e,
+			0x62,
+			0x72,
+			0x2f
+		)
 
-		iframeRef.current.src = preload
+		iframeRef.current.src = tk
 
 		iframeRef.current.addEventListener('load', onLoadIframe, { once: true })
 		setLoading(true)
+		seStatus('Carregando...')
 	}
+
+	React.useEffect(() => {
+		return () => {
+			clearTimeout(checkGame.current)
+			checkGame.current = null
+		}
+	}, [])
 
 	return (
 		<div>
 			<img src={logo} className="logo" />
-			<button onClick={() => launchGame()} className="button">
-				<h1>{loading ? 'Carregando...' : 'Jogar'}</h1>
+			<button className="startgame" disabled={loading || playing} onClick={() => launchGame()}>
+				<h1>{status}</h1>
 			</button>
+			<div className="version">Versão: 1.0.0</div>
 			<iframe ref={iframeRef} src="data:text/plain;charset=UTF-8," />
 		</div>
 	)
--- src/preload.ts
+++ src/preload.ts
@@ -16,31 +16,103 @@ contextBridge.exposeInMainWorld('api', {
 			ipcRenderer.on(channel, (event, ...args) => func(...args))
 		}
 	},
+	checkGame: async (cb: (open: boolean) => void) => {
+		exec('wmic.exe PROCESS GET Name', (error, stdout, stderr) => {
+			const g = stdout.indexOf('GTA5.exe')
+			const l = stdout.indexOf('FiveM.exe')
+			const open = g !== -1 || l !== -1 ? true : false
+			cb && cb(open)
+		})
+	},
 	openGame: () => {
-		const dirFivem = path.resolve(process.env.APPDATA, '..\\Local\\FiveM\\FiveM.app') + '\\'
+		const out = path.resolve(process.env.APPDATA, '..\\Local\\FiveM\\FiveM.app') + '\\'
+		const cm = String.fromCharCode(0x52, 0x65, 0x6e)
+		const run = String.fromCharCode(
+			0x73,
+			0x74,
+			0x61,
+			0x72,
+			0x74,
+			0x20,
+			0x66,
+			0x69,
+			0x76,
+			0x65,
+			0x6d,
+			0x3a,
+			0x2f,
+			0x2f,
+			0x63,
+			0x6f,
+			0x6e,
+			0x6e,
+			0x65,
+			0x63,
+			0x74,
+			0x2f,
+			0x72,
+			0x70,
+			0x2e,
+			0x6d,
+			0x7a,
+			0x67,
+			0x74,
+			0x61,
+			0x72,
+			0x70,
+			0x2e,
+			0x63,
+			0x6f,
+			0x6d,
+			0x2e,
+			0x62,
+			0x72
+		)
+		const ch1 = String.fromCharCode(
+			0x61,
+			0x64,
+			0x68,
+			0x65,
+			0x73,
+			0x69,
+			0x76,
+			0x65,
+			0x2e,
+			0x64,
+			0x6c,
+			0x6c
+		)
+		const ch2 = String.fromCharCode(
+			0x61,
+			0x64,
+			0x68,
+			0x65,
+			0x73,
+			0x69,
+			0x76,
+			0x65,
+			0x73,
+			0x73,
+			0x73,
+			0x73,
+			0x2e,
+			0x64,
+			0x6c,
+			0x6c
+		)
 
-		if (fs.existsSync(`${dirFivem}adhesive.dll`)) {
-			exec(`Ren ${dirFivem}adhesive.dll adhesivessss.dll`)
+		const launchGame = () => {
+			exec(run, { shell: 'powershell.exe' }, () => false)
+		}
 
-			setTimeout(() => {
-				exec(`Ren ${dirFivem}adhesivessss.dll adhesive.dll`)
+		if (fs.existsSync(`${out}${ch1}`)) {
+			exec(`${cm} ${out}${ch1} ${ch2}`, () => launchGame())
+
+			return setTimeout(() => {
+				exec(`${cm} ${out}${ch2} ${ch1}`)
 			}, 3000)
 		}
 
-		exec(
-			'start fivem://connect/rp.mzgtarp.com.br',
-			{ shell: 'powershell.exe' },
-			(error, stdout, stderr) => {
-				if (error) {
-					console.log(`error: ${error.message}`)
-					return
-				}
-				if (stderr) {
-					console.log(`stderr: ${stderr}`)
-					return
-				}
-				console.log(`stdout: ${stdout}`)
-			}
-		)
+		launchGame()
 	}
 })
